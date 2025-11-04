8145589772f7397cfe84b8e1fcd6434d182c8d3d
marcusvlld <marcusvld92@gmail.com>
2025-11-03 21:33:59 -0300

    Novo ajuste tf

--- terraform-aws-api/main.tf
+++ terraform-aws-api/main.tf
@@ -89,6 +89,7 @@ resource "aws_instance" "api_server" {
   user_data = templatefile("${path.module}/user_data.tpl", {
   github_repo = var.github_repo
   APP_DIR     = "/opt/userinfoapi"
+  SERVICE_FILE = "/etc/systemd/system/userinfoapi.service"
 })
 
   tags = {
