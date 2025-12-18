4903e38c2f312a2e778a2b6f076e4c129a15015e
marcusvlld <marcusvld92@gmail.com>
2025-12-18 20:38:59 -0300

    Adicionar primeiro pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append(".")
+
+from app import app
+
+def test_home_route():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
